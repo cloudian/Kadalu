c215b3301eea3f036c4fee8cdc994c7f42648c41
handrea2009 <37294117+handrea2009@users.noreply.github.com>
2024-01-19 16:26:29 +0100

    Update

--- csi/controllerserver.py
+++ csi/controllerserver.py
@@ -259,14 +259,10 @@ class ControllerServer(csi_pb2_grpc.ControllerServicer):
 
                 if not is_hosting_volume_free(ext_volume['name'], pvsize):
 
-                    logging.error(logf(
-                        "Hosting volume is full. Add more storage",
+                    logging.info(logf(
+                        "Hosting volume is full...Over-provisioning!",
                         volume=ext_volume['name']
                     ))
-                    errmsg = "External resource is exhausted"
-                    context.set_details(errmsg)
-                    context.set_code(grpc.StatusCode.RESOURCE_EXHAUSTED)
-                    return csi_pb2.CreateVolumeResponse()
 
                 if pvtype in [PV_TYPE_VIRTBLOCK, PV_TYPE_RAWBLOCK]:
                     vol = create_block_volume(
@@ -304,6 +300,8 @@ class ControllerServer(csi_pb2_grpc.ControllerServicer):
                     volpath=vol.volpath,
                     duration_seconds=time.time() - start_time
                 ))
+
+                update_free_size(ext_volume['name'], request.name, -pvsize)
 
                 send_analytics_tracker("pvc-external-kadalu", uid)
                 # Pass required argument to get mount working on
@@ -651,14 +649,10 @@ class ControllerServer(csi_pb2_grpc.ControllerServicer):
         # Check free-size in storage-pool before expansion
         if not is_hosting_volume_free(hostvol, additional_pvsize_required):
 
-            logging.error(logf(
-                "Hosting volume is full. Add more storage",
+            logging.info(logf(
+                "Hosting volume is full...Over-provisioning!",
                 volume=hostvol
             ))
-            errmsg = "Host volume resource is exhausted"
-            context.set_details(errmsg)
-            context.set_code(grpc.StatusCode.RESOURCE_EXHAUSTED)
-            return csi_pb2.CreateVolumeResponse()
 
         hostvoltype = existing_volume.extra['hostvoltype']
 
